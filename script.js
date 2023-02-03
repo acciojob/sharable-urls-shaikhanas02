2bc77025a77a7a78fc795a4573d2d8fef260c333
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-02-03 12:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+var url = "https://localhost:8080/"
+ function urlUpdater()
+ {
+ var params = {
+ name: document.getElementById
+("name").value ,
+ year: document.getElementById("year").
+value,
+ };
+ let cnt = 0;
+ var esc = encodeURIComponent;
+ var query = Object.keys(params)
+ .map(function(k) {if(params[k] !== "")
+ {cnt++;return esc(k) + '=' + esc(params
+[k]) + "&";}
+ })
+ .join('');
+ let t = url + "?" + query;
+ t = t.substring(0, t.length - 1);
+ document.getElementById("url").
+innerHTML = t;
+ }
